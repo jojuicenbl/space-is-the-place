ab499c77f33d417a2ac58f449e42ba4ca0e0d32c
jojuicenbl <joris.neblai@icloud.com>
2025-06-07 16:37:27 +0200

    Fix: main css bg color fix and app file restructured

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,23 +3,22 @@ import { RouterView } from 'vue-router'
 import { VApp } from 'vuetify/components'
 </script>
 <template>
-  <v-app>
-    <div class="app">
-      <main class="main-content">
-        <RouterView />
-      </main>
-    </div>
+  <v-app class="app">
+    <main class="main-content">
+      <RouterView />
+    </main>
   </v-app>
 </template>
 <style>
 .app {
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
+  background-color: var(--color-background) !important; /* Force the grey background */
 }
 
 .main-content {
-  flex: 1;
-  /* padding: 1rem; */
+  max-width: 1600px;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+  min-height: 100vh;
 }
 </style>
